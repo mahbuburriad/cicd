ab1e7f52a86b3466be669b02d242b80979e5b8de
Mahbubur Riad <mahbubur.riad@northsouth.edu>
2021-06-15 23:56:44 +0600

    changed

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
+
+// Only authenticated members can access these routes
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::get('posts/create', 'App\Http\Controllers\PostController@create')->name('posts.create');
+    Route::post('posts', 'App\Http\Controllers\PostController@store')->name('posts.store');
+    Route::get('posts/{post}/edit', 'App\Http\Controllers\PostController@edit')->name('posts.edit');
+    Route::delete('posts/{post}/destroy', 'App\Http\Controllers\PostController@destroy')->name('posts.destroy');
+    Route::patch('posts/{post}/update', 'App\Http\Controllers\PostController@update')->name('posts.update');
+
+ });
+
+ Route::get('posts', 'App\Http\Controllers\PostController@index')->name('posts.index');
+ Route::get('posts/{post}', 'App\Http\Controllers\PostController@show')->name('posts.show');
